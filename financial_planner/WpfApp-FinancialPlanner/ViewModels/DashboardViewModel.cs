9cece700a95be9b75f7643c561ebf1d718424fde
sofia_maidaniuk <ipz231_msye@student.ztu.edu.ua>
2025-05-12 00:47:03 +0300

    Додано сторінку з балансами: створення, редагування, видалення

--- financial_planner/WpfApp-FinancialPlanner/ViewModels/DashboardViewModel.cs
+++ financial_planner/WpfApp-FinancialPlanner/ViewModels/DashboardViewModel.cs
@@ -3,18 +3,39 @@ using ClassLibrary_FinancialPlanner.Models;
 using System.Collections.ObjectModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel;
 
 namespace WpfApp_FinancialPlanner.ViewModels
 {
-    public class DashboardViewModel
+    public class DashboardViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Balance> Balances { get; set; }
+        private readonly AppDbContext _context;
+        private ObservableCollection<Balance> _balances;
+
+        public ObservableCollection<Balance> Balances
+        {
+            get => _balances;
+            set
+            {
+                _balances = value;
+                OnPropertyChanged(nameof(Balances));
+            }
+        }
 
         public DashboardViewModel(AppDbContext context)
         {
-            Balances = new ObservableCollection<Balance>(
-                context.Balances.AsNoTracking().ToList()
-            );
+            _context = context;
+            LoadBalances();
         }
+
+        public void LoadBalances()
+        {
+            var list = _context.Balances.AsNoTracking().ToList();
+            Balances = new ObservableCollection<Balance>(list); 
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
